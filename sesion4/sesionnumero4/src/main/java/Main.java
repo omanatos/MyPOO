717d1afb5af6a2eaee01f8843089531967ef6617
Douglas Gomez <dmgomez@uamv.edu.ni>
2024-08-29 18:41:08 -0600

    updanjelo

--- sesion4/sesionnumero4/src/main/java/Main.java
+++ sesion4/sesionnumero4/src/main/java/Main.java
@@ -1,45 +1,17 @@
-ge models;
+import models.Calculadora;
 
-public class Circle {
-    private double radius;
-    private double circumference;
-    private double area;
+import javax.swing.*;
 
-    //Empty constructor
-    public Circle() {
-    }
+public class Main {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Bienvenido");
+        double radio, area;
+        radio = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el radio"));
+        Circle circle = new Circle(radio);
+        Calculadora calc = new Calculadora();
+        JOptionPane.showMessageDialog(null, "El area del circulo es "
+                + calc.calcularArea(circle));
 
-    public Circle(double radius, double circumference, double area) {
-        this.radius = radius;
-        this.circumference = circumference;
-        this.area = area;
-    }
-
-    public Circle(double radius) {
-        this.radius = radius;
-    }
-
-    public double getRadius() {
-        return radius;
-    }
-
-    public void setRadius(double radius) {
-        this.radius = radius;
-    }
-
-    public double getCircumference() {
-        return circumference;
-    }
-
-    public void setCircumference(double circumference) {
-        this.circumference = circumference;
-    }
-
-    public double getArea() {
-        return area;
-    }
-
-    public void setArea(double area) {
-        this.area = area;
     }
 }
+
---
+++ sesion4/sesionnumero4/src/main/java/models/Calculadora.java
@@ -0,0 +1,8 @@
+package models;
+
+public class Calculadora {
+
+    public double calcularArea(Ciecle circle){
+        return Math.PI * Math.pow(circle.getRadius(), 2)
+    }
+}
